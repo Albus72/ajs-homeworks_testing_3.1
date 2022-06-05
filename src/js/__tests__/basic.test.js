a798f1bb5fb7d99c383e231b5fb5a298fd60d6f0
Albus72 <albus72@tut.by>
2022-06-05 19:20:49 +0300

    Удалены лишние файлы user и http

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,16 +1,6 @@
-// import sum from '../basic';
-
-// test('should sum', () => {
-//   const result = sum([1, 2, 3]);
-
-//   expect(result).toBe(6);
-// });
-
 import getHeroHealthStatus from '../basic';
 
 test ('should health', () => {
   const result = getHeroHealthStatus({name: 'Маг', health: 90});
   expect(result).toBe('healthy');
 })
-
-//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
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,20 +1,3 @@
-// export default function sum(items) {
-//   let result = 0;
-//   for (const item of items) {
-//     result += item;
-//   }
-//   return result;
-// }
-
-const heroData = {name, health}
 export default function getHeroHealthStatus(heroData) {
-  let healthStatus;
-  if (heroData.health > 50) {
-    healthStatus = 'healthy';
-  } else if (15 < heroData.health <= 50) {
-    healthStatus = 'wounded';
-  } else if (heroData.health <= 15) {
-    healthStatus = 'critical';
-  }
-  return healthStatus;
+  return heroData.health <= 15 ? 'critical' : heroData.health <= 50 ? 'wounded' : 'healthy';
 }
