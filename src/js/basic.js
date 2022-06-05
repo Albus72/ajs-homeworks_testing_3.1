427176f626587adb92c0e83603701372bb3b84e6
Albus72 <albus72@tut.by>
2022-06-05 17:27:45 +0300

    Задача тесты в процессе

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,20 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+// export default function sum(items) {
+//   let result = 0;
+//   for (const item of items) {
+//     result += item;
+//   }
+//   return result;
+// }
+
+const heroData = {name, health}
+export default function getHeroHealthStatus(heroData) {
+  let healthStatus;
+  if (heroData.health > 50) {
+    healthStatus = 'healthy';
+  } else if (15 < heroData.health <= 50) {
+    healthStatus = 'wounded';
+  } else if (heroData.health <= 15) {
+    healthStatus = 'critical';
   }
-  return result;
+  return healthStatus;
 }
